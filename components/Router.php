cf3436fe8bfbecc5daa4ec12b134273edbd603cc
Gennadiy <Relect@bk.ru>
2020-06-03 11:42:11 +0300

    добавление uri запроса

--- components/Router.php
+++ components/Router.php
@@ -7,13 +7,22 @@ class Router
     public function _construct()
     {
         $routesPath= ROOT.'/config/routes.php';
+
         $this->routes = include($routesPath);
+    }
+
+    private function getURI()
+    {
+        if (!empty($_SERVER['REQUEST_URI'])){
+            return trim($_SERVER['REQUEST_URI'],'/');
+        }
     }
 
     public function run()
     {
-        print_r($this->routes);
         // Получить строку запроса
+        $uri = $this->getURI();
+        echo $uri;
 
         // Проверить наличие такого запроса в routes.php
 
